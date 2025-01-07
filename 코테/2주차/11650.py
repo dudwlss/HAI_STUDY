d4ba052468a9de781d551cc036ab5764593d0dd6
dudwlss <130740710+dudwlss@users.noreply.github.com>
2025-01-07 21:35:21 +0900

    Create 11650.py

---
+++ 코테/2주차/11650.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+points = []
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    points.append((x, y))
+
+points.sort()
+
+for x, y in points:
+    print(x, y)
